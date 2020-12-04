70e7d987c190040aefbd55c48c97a6baba083e04
Paul Realpe <paulmrg461@gmail.com>
2020-12-04 17:12:35 -0500

    11. CurvedHeader - CustomPainters

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -146,3 +146,44 @@ class _PickHeaderPainter extends CustomPainter {
     return true;
   }
 }
+
+class CurvedHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _CurvedHeaderPainter(),
+      ),
+    );
+  }
+}
+
+class _CurvedHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    //Properties
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    //paint.style = PaintingStyle.stroke;
+    //paint.strokeWidth = 20.0;
+
+    final path = Path();
+    //Draw with the path and paint
+    path.lineTo(0, size.height * 0.3);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.45, size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+    //path.lineTo(size.width, size.height * 0.3);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
