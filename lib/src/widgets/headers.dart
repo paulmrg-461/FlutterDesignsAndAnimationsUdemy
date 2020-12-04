123c73325c0e06b16af0fffa45255833151cfe82
Paul Realpe <paulmrg461@gmail.com>
2020-12-04 16:49:10 -0500

    9. TriangularHeader - CustomPainters

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -52,9 +52,49 @@ class _DiagonalHeaderPainter extends CustomPainter {
     final path = Path();
     //Draw with the path and paint
     //path.moveTo(0, size.height * 0.5);
-    path.moveTo(0, 0);
+    //path.moveTo(0, 0);
     path.lineTo(0, size.height * 0.35);
     path.lineTo(size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class TriangularHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _TriangularHeaderPainter(),
+      ),
+    );
+  }
+}
+
+class _TriangularHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //Properties
+    paint.color = Color(0xff615AAB);
+    //paint.style = PaintingStyle.stroke;
+    //paint.strokeWidth = 5.0;
+    paint.style = PaintingStyle.fill;
+
+    final path = Path();
+    //Draw with the path and paint
+    path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
     path.lineTo(0, 0);
 
