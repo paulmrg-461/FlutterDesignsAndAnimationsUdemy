7e36f29b81c1fe06da7123b362fb7ae24684d580
Paul Realpe <paulmrg461@gmail.com>
2020-12-11 18:04:16 -0500

    8. PageViewController - Change pages listener

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -22,9 +22,9 @@ class _Dots extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _Dot(),
-          _Dot(),
-          _Dot(),
+          _Dot(0),
+          _Dot(1),
+          _Dot(2),
         ],
       ),
     );
@@ -32,6 +32,9 @@ class _Dots extends StatelessWidget {
 }
 
 class _Dot extends StatelessWidget {
+  final int index;
+  _Dot(this.index);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -43,11 +46,32 @@ class _Dot extends StatelessWidget {
   }
 }
 
-class _Slides extends StatelessWidget {
+class _Slides extends StatefulWidget {
+  @override
+  __SlidesState createState() => __SlidesState();
+}
+
+class __SlidesState extends State<_Slides> {
+  final pageViewController = new PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    pageViewController
+        .addListener(() => print('Current page: ${pageViewController.page}'));
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: PageView(
+        controller: pageViewController,
         children: [
           _Slide('assets/images/slide-1.svg'),
           _Slide('assets/images/slide-2.svg'),
