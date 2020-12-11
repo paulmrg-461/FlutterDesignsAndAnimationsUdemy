964d0e424302281d19cad60df7205a2fff2643d8
Paul Realpe <paulmrg461@gmail.com>
2020-12-11 17:10:15 -0500

    6. Creating a slideshow

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -5,9 +5,37 @@ class SlideshowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset('assets/images/slide-5.svg'),
+      body: Center(child: _Slides()),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide('assets/images/slide-1.svg'),
+          _Slide('assets/images/slide-2.svg'),
+          _Slide('assets/images/slide-3.svg'),
+        ],
       ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30.0),
+      child: SvgPicture.asset(svg),
     );
   }
 }
