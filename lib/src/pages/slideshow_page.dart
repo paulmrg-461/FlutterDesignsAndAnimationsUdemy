f505cf4eb8c10f25236f8e45fb03aa38a066c5e5
Paul Realpe <paulmrg461@gmail.com>
2020-12-12 17:57:35 -0500

    18. Creating two Slideshows

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -7,19 +7,31 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.blue,
-        body: Slideshow(
-          //dotsPositionUp: true,
-          primaryColor: Colors.deepPurple,
-          secondaryColor: Colors.white,
-          dotPrimarySize: 18,
-          dotSecondarySize: 12,
-          slides: [
-            SvgPicture.asset('assets/images/slide-1.svg'),
-            SvgPicture.asset('assets/images/slide-2.svg'),
-            SvgPicture.asset('assets/images/slide-3.svg'),
-            SvgPicture.asset('assets/images/slide-4.svg'),
-            SvgPicture.asset('assets/images/slide-5.svg'),
+        body: Column(
+          children: [
+            Expanded(child: MySlideshow()),
+            Expanded(child: MySlideshow())
           ],
         ));
+  }
+}
+
+class MySlideshow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+      //dotsPositionUp: true,
+      primaryColor: Colors.deepPurple,
+      secondaryColor: Colors.white,
+      dotPrimarySize: 18,
+      dotSecondarySize: 12,
+      slides: [
+        SvgPicture.asset('assets/images/slide-1.svg'),
+        SvgPicture.asset('assets/images/slide-2.svg'),
+        SvgPicture.asset('assets/images/slide-3.svg'),
+        SvgPicture.asset('assets/images/slide-4.svg'),
+        SvgPicture.asset('assets/images/slide-5.svg'),
+      ],
+    );
   }
 }
