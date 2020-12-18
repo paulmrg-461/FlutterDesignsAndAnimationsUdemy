e401841d54d0cc9adf9f2323a6c6842f454802dc
Paul Realpe <paulmrg461@gmail.com>
2020-12-18 13:54:36 -0500

    7. Dark & Light Themes

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(
-    ChangeNotifierProvider(create: (_) => ThemeChanger(), child: MyApp()));
+    ChangeNotifierProvider(create: (_) => ThemeChanger(2), child: MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeChanger>(context);
     return MaterialApp(
+        theme: themeProvider.currentTheme,
         debugShowCheckedModeBanner: false,
         title: 'App Designs',
         home: LauncherPage());
--- lib/src/theme/theme_changer.dart
+++ lib/src/theme/theme_changer.dart
@@ -3,18 +3,48 @@ import 'package:flutter/material.dart';
 class ThemeChanger with ChangeNotifier {
   bool _darkTheme = false;
   bool _customTheme = false;
+  ThemeData _currentTheme;
+
+  ThemeChanger(int theme) {
+    switch (theme) {
+      case 1:
+        _darkTheme = false;
+        _customTheme = false;
+        _currentTheme = ThemeData.light();
+        break;
+      case 2:
+        _darkTheme = true;
+        _customTheme = false;
+        _currentTheme = ThemeData.dark();
+        break;
+      case 3:
+        _darkTheme = false;
+        _customTheme = true;
+        //_currentTheme = ThemeData.dark();
+        break;
+      default:
+        _darkTheme = false;
+        _customTheme = false;
+        _currentTheme = ThemeData.light();
+    }
+  }
 
   bool get darkTheme => this._darkTheme;
+  bool get customTheme => this._customTheme;
+  ThemeData get currentTheme => this._currentTheme;
+
   set darkTheme(bool darkTheme) {
     this._customTheme = false;
     this._darkTheme = darkTheme;
+
+    _currentTheme = darkTheme ? ThemeData.dark() : ThemeData.light();
     notifyListeners();
   }
 
-  bool get customTheme => this._customTheme;
   set customTheme(bool customTheme) {
     this._darkTheme = false;
     this._customTheme = customTheme;
+    _currentTheme = customTheme ? ThemeData.light() : ThemeData.dark();
     notifyListeners();
   }
 }
