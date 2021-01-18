a9db2f55566560f5efeb8ec598f758e66f9bce4d
Paul Realpe <paulmrg461@gmail.com>
2021-01-18 08:47:07 -0500

    4. ScaleTransition

--- lib/src/page_transitions/pages/page1.dart
+++ lib/src/page_transitions/pages/page1.dart
@@ -29,9 +29,9 @@ class Page1 extends StatelessWidget {
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           final curvedAnimation =
               CurvedAnimation(parent: animation, curve: Curves.easeInOut);
-          return SlideTransition(
-            position: Tween<Offset>(begin: Offset(-0.5, 1.0), end: Offset.zero)
-                .animate(curvedAnimation),
+
+          return ScaleTransition(
+            scale: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
             child: child,
           );
         });
