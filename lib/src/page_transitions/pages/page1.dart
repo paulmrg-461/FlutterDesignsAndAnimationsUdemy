ef9e39b461f2398bfd964693210e3f16cbb9f83d
Paul Realpe <paulmrg461@gmail.com>
2021-01-18 09:08:18 -0500

    5. RotationTransition

--- lib/src/page_transitions/pages/page1.dart
+++ lib/src/page_transitions/pages/page1.dart
@@ -25,14 +25,19 @@ class Page1 extends StatelessWidget {
         pageBuilder: (BuildContext context, Animation<double> animation,
                 Animation<double> secondaryAnimation) =>
             Page2(),
-        //transitionDuration: Duration(milliseconds: 300),
+        transitionDuration: Duration(milliseconds: 700),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           final curvedAnimation =
               CurvedAnimation(parent: animation, curve: Curves.easeInOut);
 
-          return ScaleTransition(
+          /* return ScaleTransition(
             scale: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
             child: child,
+          ); */
+
+          return RotationTransition(
+            child: child,
+            turns: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
           );
         });
   }
